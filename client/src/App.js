25cbb25fa34903c4bc6c19479c08c04090f01b61
bokyo <yung12320@daum.net>
2019-04-10 08:20:43 +0900

    Add Customer API Loading view

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
-
+import CircularProgress from '@material-ui/core/CircularProgress'
 const styles = theme => ({
   root: {
     width: '100%',
@@ -17,6 +17,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing.unit * 2
   }
 })
 
@@ -24,18 +27,24 @@ const styles = theme => ({
 
 class App extends Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress,20);
     this.callApi()
       .then(res => this.setState({customers: res}))
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+  progress = () =>{
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0: completed + 1});
   }
   render() {
     const { classes } = this.props;
@@ -55,7 +64,13 @@ class App extends Component {
           <TableBody>          
               {this.state.customers? this.state.customers.map(a => {
                 return(<Customer key={a.id} id={a.id} iamge={a.image} name={a.name} birthday={a.birthday} gender={a.gender} job={a.job}/>)
-              }) : ""}
+              }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} varient="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
