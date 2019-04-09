306985045e0004179f436b5ade1a2f7813d9909f
bokyo <yung12320@daum.net>
2019-04-09 21:03:24 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,33 +20,23 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '정보교',
-  'birthday': '010317',
-  'gender': '남자',
-  'job': '고등학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '정보교',
-  'birthday': '010317',
-  'gender': '남자',
-  'job': '고등학생'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '정보교',
-  'birthday': '010317',
-  'gender': '남자',
-  'job': '고등학생'
-}
-]
+
 
 class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err))
+  }
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -63,7 +53,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>          
-              {customers.map(a => {return(<Customer key={a.id} id={a.id} iamge={a.image} name={a.name} birthday={a.birthday} gender={a.gender} job={a.job}/>)})}
+              {this.state.customers? this.state.customers.map(a => {
+                return(<Customer key={a.id} id={a.id} iamge={a.image} name={a.name} birthday={a.birthday} gender={a.gender} job={a.job}/>)
+              }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,32 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) => {
+    res.send([{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '정보교',
+        'birthday': '010317',
+        'gender': '남자',
+        'job': '고등학생'
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '정보교',
+        'birthday': '010317',
+        'gender': '남자',
+        'job': '고등학생'
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '정보교',
+        'birthday': '010317',
+        'gender': '남자',
+        'job': '고등학생'
+      }
+      ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
